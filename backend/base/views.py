152d40d4806dd4b7e6d02e94c0c68d0be20d9535
droidroy <roy.abhik.dev@gmail.com>
2022-03-07 17:22:31 +0530

    rming pycache

--- backend/base/views.py
+++ backend/base/views.py
@@ -1,6 +1,6 @@
 from json import JSONDecodeError
 from django.shortcuts import render
-from django.http import JsonResponse
+from django.contrib.auth.models import User
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework.response import Response
@@ -29,26 +29,22 @@ class MyTokenObtainPairView(TokenObtainPairView):
     serializer_class = MyTokenObtainPairSerializer
 
 
-@api_view(['GET'])
-def getRoutes(request):
-    routes = [
-        '/api/products/',
-        '/api/products/craete/',
-        '/api/products/upload/',
-        '/api/products/<id>/reviews/',
-        '/api/products/top/',
-        '/api/products/<id>/',
-        '/api/products/delete/<id>/',
-        '/api/products/update/<id>/',
-    ]
-
-    return Response(routes)
-
+def register_user(request):
+    return Response('Data')
 
 @api_view(['GET'])
+@permission_classes([IsAuthenticated])
 def get_user_profile(request):
     user = request.user
     serializer = UserSerializer(user, many=False)
+    return Response(serializer.data)
+
+
+@api_view(['GET'])
+@permission_classes([IsAdminUser])
+def get_users(request):
+    users = User.objects.all()
+    serializer = UserSerializer(users, many=True)
     return Response(serializer.data)
 
 
