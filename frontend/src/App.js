99b9d1154c862b52f4a7acbb929bb9a4b538f30c
droidroy <roy.abhik.dev@gmail.com>
2022-03-10 18:14:48 +0530

    place order screen added
    - no payment method implemented yet

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -12,7 +12,7 @@ import RegisterScreen from './screens/RegisterScreen'
 import ProfileScreen from './screens/ProfileScreen'
 import ShippingScreen from './screens/ShippingScreen'
 import PaymentScreen from './screens/PaymentScreen'
-
+import PlaceOrderScreen from './screens/PlaceOrderScreen'
 
 
 function App() {
@@ -29,6 +29,7 @@ function App() {
 						<Route path='/profile' element={<ProfileScreen />} />
 						<Route path='/shipping' element={<ShippingScreen />} />
 						<Route path='/payment' element={<PaymentScreen />} />
+						<Route path='/placeorder' element={<PlaceOrderScreen />} />
 						<Route path='/product/:id' element={<ProductScreen />} />
 						<Route path='cart' element={<CartScreen />} >
 							<Route path=':id' element={<CartScreen />} />
