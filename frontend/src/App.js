19c8e7d8925cbf4d428b3b63ca35d43f0310cf25
droidroy <roy.abhik.dev@gmail.com>
2022-03-10 17:30:29 +0530

    payment screen implemented

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,6 +11,8 @@ import LoginScreen from './screens/LoginScreen'
 import RegisterScreen from './screens/RegisterScreen'
 import ProfileScreen from './screens/ProfileScreen'
 import ShippingScreen from './screens/ShippingScreen'
+import PaymentScreen from './screens/PaymentScreen'
+
 
 
 function App() {
@@ -26,6 +28,7 @@ function App() {
 						<Route path='/register' element={<RegisterScreen />} />
 						<Route path='/profile' element={<ProfileScreen />} />
 						<Route path='/shipping' element={<ShippingScreen />} />
+						<Route path='/payment' element={<PaymentScreen />} />
 						<Route path='/product/:id' element={<ProductScreen />} />
 						<Route path='cart' element={<CartScreen />} >
 							<Route path=':id' element={<CartScreen />} />
