32f8c3d5678f488a0d1c1a68d014705f5df43bd1
droidroy-ai <roy.abhik.dev@gmail.com>
2022-05-16 19:30:32 +0530

    product image upload function

--- backend/base/urls/product_urls.py
+++ backend/base/urls/product_urls.py
@@ -4,6 +4,7 @@ from base.views import product_views as views
 urlpatterns = [
     path('', views.get_products, name="products"),
     path('create/', views.create_product, name="product-create"),
+    path('upload/', views.upload_image, name="image-upload"),
     path('<str:pk>/', views.get_product, name="product"),
     path('update/<str:pk>/', views.update_product, name="product-update"),
     path('delete/<str:pk>/', views.delete_product, name="product-delete"),
--- frontend/src/screens/ProductEditScreen.js
+++ frontend/src/screens/ProductEditScreen.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 import { Link, useNavigate, useParams } from 'react-router-dom'
 import { useDispatch, useSelector } from 'react-redux'
 import { Form, Button } from 'react-bootstrap'
@@ -10,10 +11,27 @@ import FormContainer from '../components/FormContainer'
 import { listProductDetails, updateProduct } from '../actions/productActions'
 import { PRODUCT_UPDATE_RESET } from '../constants/productConstant'
 
+function getCookie(name) {
+    let cookieValue = null;
+    if (document.cookie && document.cookie !== '') {
+        const cookies = document.cookie.split(';');
+        for (let i = 0; i < cookies.length; i++) {
+            const cookie = cookies[i].trim();
+            // Does this cookie string begin with the name we want?
+            if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
+            }
+        }
+    }
+    return cookieValue;
+}
+const csrftoken = getCookie('csrftoken');
+
 function ProductEditScreen() {
+
     const match = useParams()
     const navigate = useNavigate()
-
     const productId = match.id
     const [name, setName] = useState('')
     const [price, setPrice] = useState(0)
@@ -22,6 +40,7 @@ function ProductEditScreen() {
     const [category, setCategory] = useState('')
     const [countInStock, setCountInStock] = useState(0)
     const [description, setDescription] = useState('')
+    const [uploading, setUploading] = useState(false)
 
     const dispatch = useDispatch()
     const productDetails = useSelector(state => state.productDetails)
@@ -68,6 +87,31 @@ function ProductEditScreen() {
         ))
     }
 
+    const uploadFileHandler = async (e) => {
+        const file = e.target.files[0]
+        const formData = new FormData()
+
+        formData.append('image', file)
+        formData.append('product_id', productId)
+        setUploading(true)
+        try {
+            const config = {
+                'headers': {
+                    'Content-Type': 'multipart/form-data',
+                    'X-CSRFToken': csrftoken
+                }
+            }
+            console.log("HIT")
+            const { data } = await axios.post('/api/products/upload/', formData, config)
+            console.log(data)
+            setImage(data)
+            setUploading(false)
+        }
+        catch(error) {
+            setUploading(false)
+        }
+    }
+
     return (
         <div>
             <Link to='/admin/productlist'>
@@ -110,6 +154,14 @@ function ProductEditScreen() {
                                 onChange={(e) => setImage(e.target.value)}
                             >
                             </Form.Control>
+                            <Form.Control
+                                label="Choose File"
+                                type="file"
+                                custom
+                                onChange={uploadFileHandler}
+                            >
+                            </Form.Control>
+                            {uploading && <Loader />}
                         </Form.Group>
 
                         <Form.Group className="mb-3" controlId='brand'>
